dc160a319a4472c85397797cff6e8910d249d4c8
Mac Pro <anhthanhcd4d2@gmail.com>
2022-02-17 10:03:48 +0700

    hoanthanh moreStaff

--- src/MinitesQuanLyNhanVien/FunctionOperation.java
+++ src/MinitesQuanLyNhanVien/FunctionOperation.java
@@ -3,17 +3,17 @@ package MinitesQuanLyNhanVien;
 import java.util.Scanner;
 
 public class FunctionOperation {
-    Scanner scanner=new Scanner(System.in);
-    protected int maNhanVien;
-    protected String name;
-    protected String gmail;
-    protected int age;
-    private int phone;
-    private int numberOf,choose,cout=0;
-    private double luongCung;
+//    Scanner scanner=new Scanner(System.in);
+//    private int maNhanVien;
+//    private String name;
+//    private String gmail;
+//    private int age;
+//    private int phone;
+//    private double luongCung;
 
     public  NhanVien[] moreStaff (NhanVien[]lissStaff){
         Scanner scanner=new Scanner(System.in);
+         int numberOf,choose;
         System.out.println("Chọn kiểu nhân viên muốn thêm: \n" +"fullTime: 1\n" +"pasTime: 2");
         System.out.println("Chọn:");
         choose=scanner.nextInt();
@@ -21,50 +21,65 @@ public class FunctionOperation {
         numberOf=scanner.nextInt();
         //tạo mảng  với  độ dài  đã nhâp
         NhanVien[]arr= new NhanVien[numberOf];
-        // chạy vòng lặp cho nhập giá trị cho đối tượng
         if (choose==1){
-            for (int i = 0; i < arr.length; i++) {
-                System.out.println("Mã Nhân viên "+(i+1)+"\t"+"tên "+(i+1)+"\t"+"gmail "+(i+1)+"\t"+"tuổi "+(i+1)+"\t"+"phone "+(i+1)+"\t");
-                maNhanVien=scanner.nextInt();
-                name=scanner.next();
-                gmail=scanner.next();
-                age=scanner.nextInt();
-                phone=scanner.nextInt();
-                // gán giá trị cho đối tượng
-                    arr[i]= new NhaVienFulLTime(maNhanVien,name,gmail,age,phone);
-                    // hiển thị thông tin nv mới
-                System.out.println("hiển thị tên mới thêm "+(i+1));
-                    System.out.println("Mã nv: "+arr[i].getMaNhanVien());
-                    System.out.println("name: "+arr[i].getName());
-                    System.out.println("gmail: "+arr[i].getGmail());
-                    System.out.println("tuổi: "+arr[i].getAge());
-                    System.out.println("phone: "+arr[i].getPhone()+"\n");
-            }
+            moreStaffFullTime(scanner, arr);
         }else if (choose==2){
-            for (int i = 0; i < arr.length; i++) {
-                System.out.println("Mã Nhân viên "+(i+1)+"\t"+"tên "+(i+1)+"\t"+"gmail "+(i+1)+"\t"+"tuổi "+(i+1)+"\t"+"phone "+(i+1)+"\t");
-                maNhanVien=scanner.nextInt();
-                name=scanner.next();
-                gmail=scanner.next();
-                age=scanner.nextInt();
-                phone=scanner.nextInt();
-                // gán giá trị cho đối tượng
-                arr[i]= new NhaVienFulLTime(maNhanVien,name,gmail,age,phone);
-                // hiển thị thông tin nv mới
-                System.out.println("hiển thị tên mới thêm "+(i+1));
-                System.out.println("Mã nv: "+arr[i].getMaNhanVien());
-                System.out.println("name: "+arr[i].getName());
-                System.out.println("gmail: "+arr[i].getGmail());
-                System.out.println("tuổi: "+arr[i].getAge());
-                System.out.println("phone: "+arr[i].getPhone()+"\n");
-            }
+            moreStaffPasTime(scanner, arr);
         }
         addArray(lissStaff,arr);
         return arr;
     }
+    private void moreStaffPasTime(Scanner scanner, NhanVien[] arr) {
+        int maNhanVien;
+        int age;
+        int phone;
+        String name;
+        String gmail;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Mã Nhân viên "+(i+1)+"\t"+"tên "+(i+1)+"\t"+"gmail "+(i+1)+"\t"+"tuổi "+(i+1)+"\t"+"phone "+(i+1)+"\t");
+            maNhanVien= scanner.nextInt();
+            name= scanner.next();
+            gmail= scanner.next();
+            age= scanner.nextInt();
+            phone= scanner.nextInt();
+            // gán giá trị cho đối tượng
+            arr[i]= new NhaVienFulLTime(maNhanVien,name,gmail,age,phone);
+            // hiển thị thông tin nv mới
+            System.out.println("hiển thị tên mới thêm "+(i+1));
+            System.out.println("Mã nv: "+ arr[i].getMaNhanVien());
+            System.out.println("name: "+ arr[i].getName());
+            System.out.println("gmail: "+ arr[i].getGmail());
+            System.out.println("tuổi: "+ arr[i].getAge());
+            System.out.println("phone: "+ arr[i].getPhone()+"\n");
+        }
+    }
+
+    private void moreStaffFullTime(Scanner scanner, NhanVien[] arr) {
+        int age;
+        String name;
+        String gmail;
+        int phone;
+        int maNhanVien;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("MãNhânviên "+(i+1)+"\t"+"tên "+(i+1)+"\t"+"gmail "+(i+1)+"\t"+"tuổi "+(i+1)+"\t"+"phone "+(i+1)+"\t");
+            maNhanVien= scanner.nextInt();
+            name= scanner.next();
+            gmail= scanner.next();
+            age= scanner.nextInt();
+            phone= scanner.nextInt();
+            // gán giá trị cho đối tượng
+                arr[i]= new NhaVienFulLTime(maNhanVien,name,gmail,age,phone);
+                // hiển thị thông tin nv mới
+            System.out.println("hiển thị tên mới thêm "+(i+1));
+                System.out.println("Mã nv: "+ arr[i].getMaNhanVien());
+                System.out.println("name: "+ arr[i].getName());
+                System.out.println("gmail: "+ arr[i].getGmail());
+                System.out.println("tuổi: "+ arr[i].getAge());
+                System.out.println("phone: "+ arr[i].getPhone()+"\n");
+        }
+    }
     public NhanVien[] addArray(NhanVien[]arr1,NhanVien[]arr2){
         //arr1 là mảng cũ, arr 2 là mảng mới thêm, result là mảng mới với độ dài của 2 mảng 1,2
-    System.out.printf("Mã nhân viên hiện có là:\t %s - %s\n",arr1[0].getMaNhanVien(),arr1[arr1.length-1].getMaNhanVien());
     int total= arr1.length+arr2.length;
     int cout=0;
     NhanVien[]result =new NhanVien[total];
@@ -74,6 +89,8 @@ public class FunctionOperation {
         }
         for (int i = 0 ; i <arr2.length ; i++) {
             result[cout]= arr2[i];
-        }return result;
+        }
+        arr1=result;
+        return arr1;
     }
 }
--- src/MinitesQuanLyNhanVien/Main.java
+++ src/MinitesQuanLyNhanVien/Main.java
@@ -6,20 +6,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
         FunctionOperation functionOperation =new FunctionOperation();
+        int size=10;
+        int choose;
+        NhanVien[]lissNhanVien= new NhanVien[size];
         // nhân viên full time;
-        NhaVienFulLTime fulLTime  =new NhaVienFulLTime(0,"343","2423423",23,965478542);
-        NhaVienFulLTime fulLTime1 =new NhaVienFulLTime(1,"Tô Việt Anh","Tovietanh@gmail.com",21,965478542);
-        NhaVienFulLTime fulLTime2 =new NhaVienFulLTime(2,"Nguyễn Thị Ánh","Nguyenthianh@gmail.com",22,965478542);
-        NhaVienFulLTime fulLTime3 =new NhaVienFulLTime(3,"Dương Xuân Bách","duongxuanbach@gmail.com",23,965478542);
-        NhaVienFulLTime fulLTime4 =new NhaVienFulLTime(4,"Nguyễn Thu Huyền","nguyenthuhuyen@gmail.com",24,965478542);
+        lissNhanVien[0] =new NhaVienFulLTime(0,"343","2423423",23,965478542);
+        lissNhanVien[1] =new NhaVienFulLTime(1,"Tô Việt Anh","Tovietanh@gmail.com",21,965478542);
+        lissNhanVien[2] =new NhaVienFulLTime(2,"Nguyễn Thị Ánh","Nguyenthianh@gmail.com",22,965478542);
+        lissNhanVien[3] =new NhaVienFulLTime(3,"Dương Xuân Bách","duongxuanbach@gmail.com",23,965478542);
+        lissNhanVien[4] =new NhaVienFulLTime(4,"Nguyễn Thu Huyền","nguyenthuhuyen@gmail.com",24,965478542);
         //nhận viên partime;
-        NhanVienParTime parTime   =new NhanVienParTime(0,"Trần Thành Đạt","Tranthanhdat@gmail.com",25,965478542);
-        NhanVienParTime parTime1  =new NhanVienParTime(1,"Nguyễn Phan Giang","Nguyenphangiang@gmail.com",24,965478542);
-        NhanVienParTime parTime2  =new NhanVienParTime(2,"Trần Quốc Khánh","Tranquockhanh@gmail.com",23,965478542);
-        NhanVienParTime parTime3  =new NhanVienParTime(3,"Lê Minh Quân","Leminhquan@gmail.com",22,965478542);
-        NhanVienParTime parTime4  =new NhanVienParTime(4,"Nguyễn Thế Cảnh","Nguyenthecanh@gmail.com",21,965478542);
-        // liss danh sách fullTime và ParTime
-        NhanVien[]lissNhanVien={fulLTime,fulLTime1,fulLTime2,fulLTime3,fulLTime4,parTime,parTime1,parTime2,parTime3,parTime4};
+        lissNhanVien[5] =new NhanVienParTime(0,"Trần Thành Đạt","Tranthanhdat@gmail.com",25,965478542);
+        lissNhanVien[6] =new NhanVienParTime(1,"Nguyễn Phan Giang","Nguyenphangiang@gmail.com",24,965478542);
+        lissNhanVien[7] =new NhanVienParTime(2,"Trần Quốc Khánh","Tranquockhanh@gmail.com",23,965478542);
+        lissNhanVien[8] =new NhanVienParTime(3,"Lê Minh Quân","Leminhquan@gmail.com",22,965478542);
+        lissNhanVien[9] =new NhanVienParTime(4,"Nguyễn Thế Cảnh","Nguyenthecanh@gmail.com",21,965478542);
+        //thêm nhân viên
+//        do{
+//            System.out.println("Danh sách chức năng: \n");
+//            System.out.println("Chức năng số 1: hiển thị danh sách nhân viên ");
+//            System.out.println("chức năng số 2: hiển thị thêm nhân viên mới ");
+//            System.out.println("chạy chức năng:\n");
+//            choose=scanner.nextInt();
+//            if (choose>5){
+//                System.out.println("vi lòng chọn đúng chức năng");}
+//        }while(choose>5);
         functionOperation.moreStaff(lissNhanVien);
     }
 
