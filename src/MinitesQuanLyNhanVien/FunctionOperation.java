30b49389e0e3671498336bd27fd1bea458c227b9
Mac Pro <anhthanhcd4d2@gmail.com>
2022-02-20 18:35:15 +0700

    add tinhluong theo ma

--- src/MinitesQuanLyNhanVien/FunctionOperation.java
+++ src/MinitesQuanLyNhanVien/FunctionOperation.java
@@ -1,12 +1,24 @@
 package MinitesQuanLyNhanVien;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FunctionOperation {
+    public static final String EMPLOYEE_MOTHER = "Mã Nhân viên ";
+    public static final String SPACE = "\t";
+    public static final String AGE = "tuổi ";
+    public static final String PHONE = "phone ";
+    public static final String GMAIL = "gmail ";
+    public static final String NAME = "tên ";
+    public static final String STAFF_NUMBER = " nhân viên số\t ";
+    public static final String STAFF_NUMBER_ADD = "Chọn số lượng nhân viên mới muốn thêm: ";
+    public static final String TYPE_STAFF = "Chọn kiểu nhân viên muốn thêm: \n";
+    public static final String SELECT_1_FULLTIME = "FULLTIME: 1\n";
+    public static final String SELECT_2_FULLTIME = "PARTIME: 2\n";
+    public static final String SELECT = "Chọn:";
+    public static final String THANK_YOU = "Cám ơn bạn đã sử dụng ";
 
     public static final String ID_STAFF = "Mã Nhân Viên: ";
-    public static final String NAME = "tên: ";
-    public static final String GMAIL = "gmail: ";
-    public static final String AGE = "tuổi: ";
-    public static final String PHONE = "phone: ";
     public static final String SHOW_POSITION = "Hiển Thị vị trí thứ %d:";
     public static final String DOWN_THE_LINE = "\n\n";
     public static final String MONEY_BASIC = "Tiền Cơ Bản: ";
@@ -15,141 +27,178 @@ public class FunctionOperation {
     public static final String HOUR = "Giờ làm";
 
     //cong 2 mang vs nhau
-    public Staff[] addArray(Staff[]listStaff, Staff[]arr2){
-        int total= listStaff.length + arr2.length;
-        int cout=0;
-        Staff[]result = new Staff[total];
+    public Staff[] addArray(Staff[] listStaff, Staff[] arr2) {
+        int total = listStaff.length + arr2.length;
+        int cout = 0;
+        Staff[] result = new Staff[total];
         for (int i = 0; i < listStaff.length; i++) {
-            result[cout]=listStaff[i];
+            result[cout] = listStaff[i];
             cout++;
         }
-        for (int i = 0 ; i <arr2.length ; i++) {
-            result[cout]= arr2[i];
+        for (int i = 0; i < arr2.length; i++) {
+            result[cout] = arr2[i];
             cout++;
         }
         return result;
     }
+
     // hiển thị danh sách  nhân viên
-    public void showEntireList(Staff[]listStaff){
+    public void showEntireList(Staff[] listStaff) {
         for (int i = 0; i < listStaff.length; i++) {
-            if (listStaff[i] instanceof StaffFullTime){
-            System.out.printf(SHOW_POSITION + DOWN_THE_LINE,(i+1));
-            System.out.println(ID_STAFF +listStaff[i].getMaNhanVien());
-            System.out.println(NAME +listStaff[i].getName());
-            System.out.println(GMAIL +listStaff[i].getGmail());
-            System.out.println(AGE +listStaff[i].getAge());
-            System.out.println(PHONE +listStaff[i].getPhone());
-            System.out.println(MONEY_BASIC +((StaffFullTime)listStaff[i]).getLuongCung());
-            System.out.println(MONEY_BONUS +((StaffFullTime)listStaff[i]).getTienThuong());
-            System.out.println(MONEY_PUNISH +((StaffFullTime)listStaff[i]).getTienPhat());
-            }if (listStaff[i] instanceof StaffParTime){
-                System.out.printf(SHOW_POSITION + DOWN_THE_LINE,(i+1));
-                System.out.println(ID_STAFF +listStaff[i].getMaNhanVien());
-                System.out.println(NAME +listStaff[i].getName());
-                System.out.println(GMAIL +listStaff[i].getGmail());
-                System.out.println(AGE +listStaff[i].getAge());
-                System.out.println(PHONE +listStaff[i].getPhone());
-                System.out.println(HOUR +((StaffParTime)listStaff[i]).getHour());
+            if (listStaff[i] instanceof StaffFullTime) {
+                System.out.printf(SHOW_POSITION + DOWN_THE_LINE, (i + 1));
+                System.out.println(ID_STAFF + listStaff[i].getMaNhanVien());
+                System.out.println(NAME + listStaff[i].getName());
+                System.out.println(GMAIL + listStaff[i].getGmail());
+                System.out.println(AGE + listStaff[i].getAge());
+                System.out.println(PHONE + listStaff[i].getPhone());
+                System.out.println(MONEY_BASIC + ((StaffFullTime) listStaff[i]).getLuongCung());
+                System.out.println(MONEY_BONUS + ((StaffFullTime) listStaff[i]).getTienThuong());
+                System.out.println(MONEY_PUNISH + ((StaffFullTime) listStaff[i]).getTienPhat());
+            }
+            if (listStaff[i] instanceof StaffParTime) {
+                System.out.printf(SHOW_POSITION + DOWN_THE_LINE, (i + 1));
+                System.out.println(ID_STAFF + listStaff[i].getMaNhanVien());
+                System.out.println(NAME + listStaff[i].getName());
+                System.out.println(GMAIL + listStaff[i].getGmail());
+                System.out.println(AGE + listStaff[i].getAge());
+                System.out.println(PHONE + listStaff[i].getPhone());
+                System.out.println(HOUR + ((StaffParTime) listStaff[i]).getHour());
             }
         }
     }
     //tính Tông Tiền toàn bộ fulltime
-    public int salaryOfAllEmployeesFullTime(Staff[]listStaff){
-        int averageWage=0;
-        int count=0;
-        for (int i = 0; i < listStaff.length ;i++) {
-            if(listStaff[i]instanceof StaffFullTime){
-               averageWage += listStaff[i].tinhLuong();
-                count++;
-            }
-        }
-        return  averageWage;
-    }
-    //tính Tông Tiền toàn bộ partime
-    public int salaryOfAllEmployeesParTime(Staff[]listStaff){
-        int averageWage=0;
-        int count=0;
-        for (int i = 0; i < listStaff.length ;i++) {
-            if(listStaff[i]instanceof StaffParTime){
+    public int salaryOfAllEmployeesFullTime(Staff[] listStaff) {
+        int averageWage = 0;
+        int count = 0;
+        for (int i = 0; i < listStaff.length; i++) {
+            if (listStaff[i] instanceof StaffFullTime) {
                 averageWage += listStaff[i].tinhLuong();
                 count++;
             }
         }
-        return  averageWage;
+        return averageWage;
     }
-    // Tính trung bình full time
-    public double averagedWageFullTime(Staff[]listStaff) {
-        int count= 0;
-        double medium=0;
-        for (int i = 0; i < listStaff.length ;i++) {
-            if(listStaff[i] instanceof StaffFullTime){
+    //tính Tông Tiền toàn bộ partime
+    public int salaryOfAllEmployeesParTime(Staff[] listStaff) {
+        int averageWage = 0;
+        int count = 0;
+        for (int i = 0; i < listStaff.length; i++) {
+            if (listStaff[i] instanceof StaffParTime) {
+                averageWage += listStaff[i].tinhLuong();
                 count++;
             }
-        } medium += salaryOfAllEmployeesFullTime(listStaff)/count;
+        }
+        return averageWage;
+    }
+
+    // Tính trung bình full time
+    public double averagedWageFullTime(Staff[] listStaff) {
+        int count = 0;
+        double medium = 0;
+        for (int i = 0; i < listStaff.length; i++) {
+            if (listStaff[i] instanceof StaffFullTime) {
+                count++;
+            }
+        }
+        medium += salaryOfAllEmployeesFullTime(listStaff)/count;
         return medium;
     }
+
     // Tính trung bình par time
     public double averagedWageParTime(Staff[] listStaff) {
-        int count= 0;
-        double medium=0;
-        for (int i = 0; i < listStaff.length ;i++) {
-            if(listStaff[i] instanceof StaffParTime){
+        int count = 0;
+        double medium = 0;
+        for (int i = 0; i < listStaff.length; i++) {
+            if (listStaff[i] instanceof StaffParTime) {
                 count++;
             }
-        } medium += salaryOfAllEmployeesParTime(listStaff)/count;
+        }
+        medium += salaryOfAllEmployeesParTime(listStaff) / count;
         return medium;
     }
+
     // liệt kê nhân viên có lương thấp hơn mức trung bình fulTime
-    public  void lowSalaryFullTimeEmployee(Staff[]listStaff){
-        for (int i = 0; i < listStaff.length ;i++) {
-            if(listStaff[i] instanceof StaffFullTime){
-                double total = ((StaffFullTime) listStaff[i]).getLuongCung()+(((StaffFullTime) listStaff[i]).getTienThuong()-((StaffFullTime) listStaff[i]).getTienPhat());
-                if (averagedWageFullTime(listStaff)<=total){
-                    System.out.println(SHOW_POSITION+(i+1));
-                    System.out.println(ID_STAFF+ listStaff[i].getMaNhanVien());
-                    System.out.println(NAME+ listStaff[i].getName());
-                    System.out.println(MONEY_BASIC +((StaffFullTime) listStaff[i]).getLuongCung());
-                    System.out.println(MONEY_BONUS +((StaffFullTime) listStaff[i]).getTienThuong());
-                    System.out.println(MONEY_PUNISH +((StaffFullTime) listStaff[i]).getTienPhat());
-                };
+    public void lowSalaryFullTimeEmployee(Staff[] listStaff) {
+        for (int i = 0; i < listStaff.length; i++) {
+            if (listStaff[i] instanceof StaffFullTime) {
+                double total = ((StaffFullTime) listStaff[i]).getLuongCung() + (((StaffFullTime) listStaff[i]).getTienThuong() - ((StaffFullTime) listStaff[i]).getTienPhat());
+                if (averagedWageFullTime(listStaff) <= total) {
+                    System.out.println(SHOW_POSITION + (i + 1));
+                    System.out.println(ID_STAFF + listStaff[i].getMaNhanVien());
+                    System.out.println(NAME + listStaff[i].getName());
+                    System.out.println(MONEY_BASIC + ((StaffFullTime) listStaff[i]).getLuongCung());
+                    System.out.println(MONEY_BONUS + ((StaffFullTime) listStaff[i]).getTienThuong());
+                    System.out.println(MONEY_PUNISH + ((StaffFullTime) listStaff[i]).getTienPhat());
+                }
             }
         }
     }
+
     // nhập lương cho nhân viên full time
-    public void nhapTienChoNhanVienFullTime(Staff[]listStaff, int basic ,int bonus, int  punish){
-            for (int i = 0; i < listStaff.length ;i++) {
-                if (listStaff[i] instanceof StaffFullTime) {
-                    ((StaffFullTime) listStaff[i]).setLuongCung(basic);
-                    ((StaffFullTime) listStaff[i]).setTienPhat(punish);
-                    ((StaffFullTime) listStaff[i]).setTienThuong(bonus);
-                }
+    public void nhapTienChoNhanVienFullTime(Staff[] listStaff, int basic, int bonus, int punish) {
+        for (int i = 0; i < listStaff.length; i++) {
+            if (listStaff[i] instanceof StaffFullTime) {
+                ((StaffFullTime) listStaff[i]).setLuongCung(basic);
+                ((StaffFullTime) listStaff[i]).setTienPhat(punish);
+                ((StaffFullTime) listStaff[i]).setTienThuong(bonus);
+            }
         }
     }
+
     // nhập lương cho nhân viên partime
-    public void nhapTienChoNhanVienParTime(Staff[]listStaff,double hour){
-        for (int i = 0; i < listStaff.length ;i++) {
+    public void nhapTienChoNhanVienParTime(Staff[] listStaff, double hour) {
+        for (int i = 0; i < listStaff.length; i++) {
             if (listStaff[i] instanceof StaffParTime) {
                 ((StaffParTime) listStaff[i]).setHour(hour);
             }
         }
     }
+    // arraylist
+    //hiển thi
+    public void showlistStaff(ArrayList<Staff> arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(STAFF_NUMBER + (i + 1));
+            System.out.println(EMPLOYEE_MOTHER + arrayList.get(i).getMaNhanVien());
+            System.out.println(NAME + arrayList.get(i).getName());
+            System.out.println(GMAIL + arrayList.get(i).getGmail());
+            System.out.println(AGE + arrayList.get(i).getAge());
+            System.out.println(PHONE + arrayList.get(i).getPhone());
+            System.out.println(arrayList.get(i));
+            // hoi tai sao ghi chi show arraylist.get se hien day du thong tin ma k hien
+        }
+    }
+    // tính tiền từng nhân viên
+    public void salaryPerPerson(ArrayList<Staff>arrayList,int maNhanVien, int choose){
+        double money=0;
+        if (choose==1) {
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (arrayList.get(i) instanceof StaffFullTime) {
+                    int check = arrayList.get(i).getMaNhanVien();
+                    if (maNhanVien == check) {
+                        System.out.println(arrayList.get(i));
+                        System.out.println(arrayList.get(i).tinhLuong());
+                    }
+                }
+            }
+        }
+        if (choose==2){
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (arrayList.get(i) instanceof StaffParTime) {
+                    int check = arrayList.get(i).getMaNhanVien();
+                    if (maNhanVien == check) {
+                        System.out.println(arrayList.get(i));
+                        System.out.println(arrayList.get(i).tinhLuong());
+                    }
+                }
+            }
+        }
+    }
+    public void sapXepTienLuong(Staff[]listStaff){
+        ArrayList<Staff> newlist = new ArrayList<>(Arrays.asList(listStaff));//chuyển arr thành ArrayList
+        for (int i = 0; i < newlist.size(); i++) {
+            newlist.sort((o1, o2) -> o1.getMaNhanVien()-o2.getMaNhanVien() );// hoi chi kieeu anh cach hoat dong cua  xap xep mang,
+            System.out.println(newlist.get(i));
+        }
+    }
 }
-//    public void xapXepLuongFullTimeTangLen(Staff[]listStaff){
-//        int cout=0;
-//        for (int i = 0; i < listStaff.length; i++) {
-//            if (listStaff[i] instanceof StaffFullTime){
-//                    cout++;
-//                }
-//        }//5,7,3,9,2,6,8
-//        Staff[]arr = new Staff[5];
-//        double temp = listStaff[0].tinhLuong();
-//            for (int i = 0; i < cout; i++) {
-//                if (listStaff[i] instanceof StaffFullTime){
-//                    if ( temp>listStaff[i+1].tinhLuong()){
-//                        temp=listStaff[i+1].tinhLuong();
-//                        arr[i]=listStaff[i+1];
-//                    }
-//                }
-//            }
-//    }
-//}
--- src/MinitesQuanLyNhanVien/Main.java
+++ src/MinitesQuanLyNhanVien/Main.java
@@ -1,8 +1,12 @@
 package MinitesQuanLyNhanVien;
 
+import MinitesQuanLyNhanVien.FunctionOperation;
+import MinitesQuanLyNhanVien.Staff;
+import MinitesQuanLyNhanVien.StaffFullTime;
+import MinitesQuanLyNhanVien.StaffParTime;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -15,15 +19,18 @@ public class Main {
     public static final String STAFF_NUMBER = " nhân viên số\t ";
     public static final String STAFF_NUMBER_ADD = "Chọn số lượng nhân viên mới muốn thêm: ";
     public static final String TYPE_STAFF = "Chọn kiểu nhân viên muốn thêm: \n";
+    public static final String TYPE_STAFF1 = "Chọn kiểu nhân viên Tính Lương: \n";
     public static final String SELECT_1_FULLTIME = "FULLTIME: 1\n";
     public static final String SELECT_2_FULLTIME = "PARTIME: 2\n";
     public static final String SELECT = "Chọn:";
+    public static final String SELECT1 = "Chọn ứng Dụng : ";
     public static final String THANK_YOU = "Cám ơn bạn đã sử dụng ";
+    public static final String RESELECT = "Vui Lòng trọn đúng ứng dụng";
+    public static final String THONG_BAO = "Chọn 1 để sử dụng 21 để thoát ứng dụng";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         FunctionOperation functionOperation = new FunctionOperation();
-        ArrayList<Staff> newStaff= new ArrayList<Staff>();
         int size = 10;
         Staff[] listStaff = new Staff[size];
         // nhân viên full time;
@@ -33,15 +40,29 @@ public class Main {
         listStaff[3] = new StaffFullTime(3, "Dương Xuân Bách", "duongxuanbach@gmail.com", 23, "965478542");
         listStaff[4] = new StaffFullTime(4, "Nguyễn Thu Huyền", "nguyenthuhuyen@gmail.com", 24, "965478542");
         //nhận viên partime;
+
         listStaff[5] = new StaffParTime(0, "Trần Thành Đạt", "Tranthanhdat@gmail.com", 25, "965478542");
         listStaff[6] = new StaffParTime(1, "Nguyễn Phan Giang", "Nguyenphangiang@gmail.com", 24, "965478542");
         listStaff[7] = new StaffParTime(2, "Trần Quốc Khánh", "Tranquockhanh@gmail.com", 23, "965478542");
         listStaff[8] = new StaffParTime(3, "Lê Minh Quân", "Leminhquan@gmail.com", 22, "965478542");
         listStaff[9] = new StaffParTime(4, "Nguyễn Thế Cảnh", "Nguyenthecanh@gmail.com", 21, "965478542");
         // mảng lưu danh sách  bảng lương nhân viên passTime và FullTime;
+        System.out.println("chọn ứng dụng 0- hiển thị danh sách nhân viên ");
+        System.out.println("chọn ứng dụng 1- thêm nhân viên mới ");
+        System.out.println("chọn ứng dụng 2- Tính tiền toàn công ty  ");
+        System.out.println("chọn ứng dụng 3- tính trung bình farTime ");
+        System.out.println("chọn ứng dụng 4- tính trung bình fullTime  ");
+        System.out.println("chọn ứng dụng 5- liệt kê nhân viên có lương thấp hơn mức trung bình fulTime ");
+        System.out.println("chọn ứng dụng 6- Nhập Lương cho nhân viên ");
+        System.out.println("chọn ứng dụng 7- thêm nhân viên mới theo kiểu arraylis ");
+        System.out.println("chọn ứng dụng 8- hiển thị ");
+        System.out.println("chọn ứng dụng 9- sắp xếp thứ tự lương ");
+        System.out.println("chọn ứng dụng 10- tính tiền từng nhân viên theo mã nhân viên ");
+        System.out.println("chọn ứng dụng 11- đóng ứng dụng ");
         int check;
+        //thêm nhân viên mới
         do {
-            System.out.println(SELECT);
+            System.out.println(SELECT1);
             check = scanner.nextInt();
             if (check == 0) {
                 functionOperation.showEntireList(listStaff);
@@ -86,8 +107,8 @@ public class Main {
                 // CHO  mảng cũ tham chiếu đến mảng nhân viên mới;
                 listStaff = functionOperation.addArray(listStaff, listStarff);
             }
-//             tính tiền toàn công ty
             if (check == 2) {
+                //   tính tiền toàn công ty
                 int total = functionOperation.salaryOfAllEmployeesFullTime(listStaff) +
                         functionOperation.salaryOfAllEmployeesParTime(listStaff);
                 System.out.println(total);
@@ -117,7 +138,6 @@ public class Main {
                     // nhập lương cho nhân viên fulltime
                     for (int i = 0; i < listStaff.length; i++) {
                         if (listStaff[i] instanceof StaffFullTime) {
-                            System.out.println(STAFF_NUMBER + (i + 1));
                             System.out.println(EMPLOYEE_MOTHER + listStaff[i].getMaNhanVien());
                             System.out.println(NAME + listStaff[i].getName());
                             basic = scanner.nextInt();
@@ -131,7 +151,6 @@ public class Main {
                     // nhập lương cho nhân viên pastime
                     for (int i = 0; i < listStaff.length; i++) {
                         if (listStaff[i] instanceof StaffParTime) {
-                            System.out.println(STAFF_NUMBER + (i + 1));
                             System.out.println(EMPLOYEE_MOTHER + listStaff[i].getMaNhanVien());
                             System.out.println(NAME + listStaff[i].getName());
                             hour = scanner.nextDouble();
@@ -140,10 +159,11 @@ public class Main {
                     }
                 }
             }
-            if (check==7){
-                // thêm mới kiểu arrraylist;
-                //chạy kiểu ArrayList
-                System.out.println(newStaff.size());
+            // LÀM THEO ArrayList
+            if (check == 7) {
+                // them mới nhân viên
+                ArrayList<Staff> addnewStaff = new ArrayList<Staff>();
+                System.out.println(addnewStaff.size());
                 int numberOf, choose;
                 System.out.println(TYPE_STAFF + SELECT_1_FULLTIME + SELECT_2_FULLTIME);
                 System.out.println(SELECT);
@@ -156,6 +176,7 @@ public class Main {
                 String phone;
                 int maNhanVien;
                 if (choose == 1) {
+                    //cho mặc định ấn 1 lần là thêm 1  nhân viên :))
                     for (int i = 0; i < 1; i++) {
                         System.out.println(STAFF_NUMBER + (i + 1));
                         System.out.println(EMPLOYEE_MOTHER + SPACE + NAME + SPACE + GMAIL + SPACE + AGE + SPACE + PHONE + SPACE);
@@ -164,7 +185,7 @@ public class Main {
                         gmail = scanner.next();
                         age = scanner.nextInt();
                         phone = scanner.next();
-                        newStaff.add(new StaffFullTime(maNhanVien,name,gmail,age,phone));
+                        addnewStaff.add(new StaffFullTime(maNhanVien, name, gmail, age, phone));
                     }
                 } else if (choose == 2) {
                     for (int i = 0; i < 1; i++) {
@@ -175,41 +196,38 @@ public class Main {
                         gmail = scanner.next();
                         age = scanner.nextInt();
                         phone = scanner.next();
-                        newStaff.add(new StaffParTime(maNhanVien, name, gmail, age, phone));
+                        addnewStaff.add(new StaffParTime(maNhanVien, name, gmail, age, phone));
                     }
                 }
                 // tạo 1 mảng mới lớp nhân viên sau đó cho độ dài bằng mảng ArrayList
-                Staff[]listStraffnew= new Staff[newStaff.size()];
+                Staff[] listStraffnew = new Staff[addnewStaff.size()];
                 // dùng phương pháp toString để đổi mảng ArrayList thành listStraffnew
-                newStaff.toArray(listStraffnew);
+                addnewStaff.toArray(listStraffnew);
                 // cộng mảng  CHO  mảng cũ tham chiếu đến mảng nhân viên mới;
-                listStaff = functionOperation.addArray(listStaff,listStraffnew);
+                listStaff = functionOperation.addArray(listStaff, listStraffnew);
             }
-            if (check==8){
+            if (check == 8) {
                 //hiển thị danh sách nhân viên
-                ArrayList<Staff> newlist= new ArrayList<>(Arrays.asList(listStaff));
-                for (int i = 0; i < newlist.size(); i++) {
-                    System.out.println(STAFF_NUMBER + (i + 1));
-                    System.out.println(EMPLOYEE_MOTHER+newlist.get(i).getMaNhanVien());
-                    System.out.println(NAME+newlist.get(i).getName());
-                    System.out.println(GMAIL+newlist.get(i).getGmail());
-                    System.out.println(AGE+newlist.get(i).getAge());
-                    System.out.println(PHONE+newlist.get(i).getPhone());
-                    System.out.println(newlist.get(i));
-                }
+                ArrayList<Staff> newlistStaff = new ArrayList<>(Arrays.asList(listStaff));// chuyển arr thành ArrayList
+                functionOperation.showlistStaff(newlistStaff);
             }
-            if (check==9){
-                ArrayList<Staff> newlist= new ArrayList<>(Arrays.asList(listStaff));
-                for (int i = 0; i <newlist.size() ; i++) {
-                    System.out.println(newlist.get(i).tinhLuong());
-                    newlist.get(i).tinhLuong();
-                    Staff[] finalListStaff = listStaff;
-                    newlist.sort((o1, o2) -> functionOperation.salaryOfAllEmployeesFullTime(finalListStaff)- functionOperation.salaryOfAllEmployeesParTime(finalListStaff));
-                    // hoi chi kieeu anh cach hoat dong cua  xap xep mang,
-                    System.out.println(newlist.get(i).getName());
-                }
+            if (check == 9) {
+                //chưa hoàn thành
+                functionOperation.sapXepTienLuong(listStaff);
             }
-        } while (check < 20);
+            if (check == 10) {
+                // tính  tiền từng nhân viên theo mã nhân viên
+                int maNhanVien;
+                int choose;
+                System.out.println(TYPE_STAFF1 + SELECT_1_FULLTIME + SELECT_2_FULLTIME);
+                System.out.println(SELECT);
+                choose = scanner.nextInt();
+                System.out.println(EMPLOYEE_MOTHER);
+                maNhanVien = scanner.nextInt();
+                ArrayList<Staff> newlistStaff = new ArrayList<>(Arrays.asList(listStaff));
+                functionOperation.salaryPerPerson(newlistStaff, maNhanVien, choose);
+            }
+        } while (check < 11);
         System.out.println(THANK_YOU);
     }
 }
